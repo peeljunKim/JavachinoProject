96f84f25c11aecb6b47f71099b5df9bbb301b925
peeljunKIM <kimkr7410@naver.com>
2023-07-29 19:47:41 +0900

    javachino project 초기 설정
    폴더, dependency 초기 설정

---
+++ javachino/src/main/java/com/example/demo/JavachinoApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavachinoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavachinoApplication.class, args);
+	}
+
+}
