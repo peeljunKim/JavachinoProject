87fb8c3ffc181efc8bc2a93999c0bec2f8330608
EunnyKoo <kooeun55555@gmail.com>
2023-08-15 19:25:33 +0900

    payactivity 수정

--- javachino/src/main/java/com/example/demo/mypage/service/PayActivityService.java
+++ javachino/src/main/java/com/example/demo/mypage/service/PayActivityService.java
@@ -1,10 +1,13 @@
 package com.example.demo.mypage.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.example.demo.entity.ActivityRv;
+import com.example.demo.entity.View_PayAccomodation;
 import com.example.demo.entity.View_PayActivity;
 import com.example.demo.mypage.dao.PayActivityDAO;
 
