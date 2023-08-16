b828f73789ed52f0e8fcf59100dfe52d6d9a9960
chj12 <hyunjucho12@gmail.com>
2023-08-16 18:04:19 +0900

    accomodation 부분 삭제

--- javachino/src/main/java/com/example/demo/accomodation/repository/UsersRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.accomodation.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.demo.entity.Users;
-
-public interface UsersRepository extends JpaRepository<Users, Integer> {
-	public Users findByUsersNo(int usersNo);
-}
